00b65b8020b10c0641b6a8143073dece886a4b9f
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-23 04:58:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -30,9 +30,12 @@ def third_challenge
       :age => "51"
         },
       :heroine => {
-        
+        :name => "Juliet",
+      :age => "15",
+      :status => "alive"
         },
       :heroine_friends => []
+      
    }
   }
 
