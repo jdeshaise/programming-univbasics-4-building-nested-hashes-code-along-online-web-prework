73f01836365d0cfc2b26495f69262ea76e67c943
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-23 04:51:35 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-     
+     :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
       }, 
     :capulet => {
       #your key/value pairs here!
