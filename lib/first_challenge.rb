68720b0c2c77d1c8b108310dc2cdb5795fbc0d49
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-23 04:40:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,10 @@ def first_challenge
    epic_tragedy = movie title  
    { ROmeo + Juliet 
    
-   cast: Leonardo DiCaprio [ ] 
+   cast: "Leonardo DiCaprio" 
+   character => "ROmeo"
+   "ROmeo's attrilbutes"
+   => romatic 
      
   cast  [Juliet = ]
      
